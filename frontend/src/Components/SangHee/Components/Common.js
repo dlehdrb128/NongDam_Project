011c219e87f01a20d9291c92b446ea0ed5a6b308
YANGSANGHEE <109570278+YANGSANGHEE@users.noreply.github.com>
2022-09-29 19:04:30 +0900

    intro 90% 완료

---
+++ frontend/src/Components/SangHee/Components/Common.js
@@ -0,0 +1,15 @@
+export const Common = {
+  Maincol: 'rgba(128,195,66,1)',
+  placeholder: 'rgba(149,149,149,1)',
+  text: 'rgba(82,82,82,1)',
+  bar: 'rgba(192,192,192,1)',
+  orange: 'rgba(243,152,0,1)',
+  red: 'rgba(255,0,0,1)',
+  gray: 'rgba(242,242,242,1)',
+  white: 'rgba(255,255,255,1)',
+  fontSize_60: '6rem',
+  fontSize_50: '5rem',
+  fontSize_30: '3rem',
+  fontSize_25: '2.5rem',
+  fontSize_20: '2rem',
+}
--- frontend/src/Components/SangHee/Components/Intro.js
+++ frontend/src/Components/SangHee/Components/Intro.js
@@ -1,9 +1,36 @@
 import '../../../App/App.css'
-import styled from 'styled-components';
+import styled from 'styled-components'
+import IntroTopvisual from './IntroTopvisual';
+import Introsec1 from './section/Introsec1';
+import Introsec2 from './section/Introsec2';
+import Introsec3 from './section/Introsec3';
+import Introsec4 from './section/Introsec4';
+
+const Main = styled.main`
+  width: inherit;
+  padding :200px 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction:column;
+  &>div.sec_wrap{
+    width:1280px;
+  }
+`
 
 const Intro = () => {
   return (
-    <></>
+    <>
+      <IntroTopvisual />
+      <Main>
+        <div className='sec_wrap'>
+          <Introsec1 />
+          <Introsec2 />
+          <Introsec3 />
+          <Introsec4 />
+        </div>
+      </Main>
+    </>
   );
 };
 
