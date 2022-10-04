409adda459fd2fa53fef6edb6a974057e2fc2f45
geunsoo <geunsoo1106@gmail.com>
2022-10-04 10:55:05 +0900

    회원가입 완료창 최종본

--- frontend/src/Components/Geunsoo/Components/signUpEnd.js
+++ frontend/src/Components/Geunsoo/Components/signUpEnd.js
@@ -1,31 +1,55 @@
 import styled from "styled-components";
 import "../../../../src/index.css";
 import Theme from "../../../theme/theme";
-// 가장 진한 회색 rgba(82,82,82,1)
-// 중간 회색 rgba(149,149,149,1)
-// 가장 옅은 회색 rgba(192,192,192,1)
-// 초록색 rgba(128,195,66,1)
-// 빨간색 rgba(255,0,0,1)
-// 오렌지색 rgba(243,152,0,1)
-// ?? rgba(242,242,242,1)
-
 const SignUpEnd = () => {
   const SignUpEndBox = styled.div`
     width: 1230px;
+    height: 400px;
     display: flex;
     flex-direction: column;
-    justify-content: center;
+    justify-content: space-around;
     align-items: center;
+    margin-top: 100px;
+    & > h2 {
+      font-size: 2rem;
+      font-family: NanumGothic-ExtraBold;
+      color: ${Theme.green};
+    }
+    & > div:nth-child(2) > img {
+      width: 150px;
+    }
+    & > div:nth-child(4) {
+      text-align: center;
+      font-size: 1.5rem;
+      font-family: NanumGothic-Regular;
+      color: ${Theme.gray};
+    }
+    & > a:nth-child(5) {
+      text-decoration: none;
+      width: 150px;
+      height: 50px;
+      border-radius: 10px;
+      background-color: ${Theme.green};
+      color: ${Theme.realWhite};
+      display: flex;
+      font-size: 1.5rem;
+      font-family: NanumGothic-ExtraBold;
+      justify-content: center;
+      align-items: center;
+    }
   `;
   const SignUpEndHead = styled.div`
     font-size: 3rem;
+    font-family: lotteHappyBold;
     color: ${Theme.lightBlack};
   `;
   return (
     <>
       <SignUpEndBox>
         <SignUpEndHead>회원 가입 완료</SignUpEndHead>
-        <div></div>
+        <div>
+          <img src="/img/logo-2.svg" alt="이미지 없음"></img>
+        </div>
         <h2>회원가입이 정상적으로 완료되었습니다 !</h2>
         <div>
           농담 회원 가입을 축하드립니다.
