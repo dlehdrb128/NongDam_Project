33e65b72e26d73c57d69aa771af128ce3b32a2a6
YANGSANGHEE <109570278+YANGSANGHEE@users.noreply.github.com>
2022-10-06 12:32:08 +0900

    Mypage 주석완료

--- frontend/src/Components/SangHee/Components/MyPage/MyPage.js
+++ frontend/src/Components/SangHee/Components/MyPage/MyPage.js
@@ -2,12 +2,14 @@ import styled from 'styled-components';
 import Mypagesec1 from './section/Mypagesec1';
 import Mypagesec2 from './section/Mypagesec2';
 
+// 메인 컨테이너
 const Main = styled.main`
   width: inherit;
   padding-top: 130px;
   display: flex;
   align-items: center;
   justify-content: center;
+     //섹션 컨테이너
   & > div.sec_wrap {
     position: relative;
     display: flex;
@@ -21,7 +23,9 @@ const Mypage = () => {
     <Main>
       <div className='sec_wrap'>
         <Mypagesec1 />
+        {/* section1/왼쪽메뉴 */}
         <Mypagesec2 />
+        {/* section2/오른쪽 상세내역 */}
       </div>
     </Main>
   );
