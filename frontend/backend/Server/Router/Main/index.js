c3216f73dbec94e04abfd98f786f87552e8d9927
4LEAF-LSH <4leaf.lsh@gmail.com>
2022-10-07 15:23:28 +0900

    test

--- frontend/backend/Server/Router/Main/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/", (req, res) => {
-  res.send("하이");
-});
-
-module.exports = router;
--- frontend/frontend/src/Components/Sangho/Components/orderDetail.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import styled from "styled-components";
-import Theme from "../../../theme/theme";
-
-const OrderDetail = () => {
-  return <div>asdad</div>;
-};
-
-export default OrderDetail;
