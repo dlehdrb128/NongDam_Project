a388f2d969189f3058b8319b6e1d7db16e0410e2
Yehoonn <jes6208@naver.com>
2022-10-20 10:04:38 +0900

    'productpage-update'

--- frontend/src/Pages/ProductPage/ProductPageTop.js
+++ frontend/src/Pages/ProductPage/ProductPageTop.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 import { useState } from "react";
 import { StyledButton, Theme } from "../../Theme/theme";
 import axios from "axios";
@@ -31,10 +32,22 @@ const ProductPageTopRight = styled.div`
   height: 100%;
 
   /* 스토어 이름 */
-  & > div:nth-child(1) {
-    font-family: "SCD-3";
-    font-size: 2rem;
-    color: ${({ theme }) => theme.lightblack};
+  & > a {
+    & > div:nth-child(1) {
+      width: 200px;
+      font-family: "SCD-3";
+      font-size: 2rem;
+      color: ${({ theme }) => theme.lightblack};
+
+      &:hover {
+        font-weight: bold;
+        cursor: pointer;
+      }
+    }
+
+    &:hover {
+      cursor: unset;
+    }
   }
 
   /* 지역 */
@@ -184,6 +197,7 @@ const ProductPageTop = ({ ProductData }) => {
   const data = [ProductData[0], count];
   let salePrice = null;
 
+  console.log(ProductData);
   const up = () => {
     if (count > 0) {
       setCount((count) => count - 1);
@@ -216,7 +230,9 @@ const ProductPageTop = ({ ProductData }) => {
         ></img>
       </ProductPageTopLeft>
       <ProductPageTopRight>
-        <div>{ProductData.store_name}</div>
+        <Link to={`/store/${ProductData.store_key}`}>
+          <div>{ProductData.store_name}</div>
+        </Link>
         <div>[{ProductData.product_local}]</div>
         <div>{ProductData.product_name}</div>
         <hr></hr>
