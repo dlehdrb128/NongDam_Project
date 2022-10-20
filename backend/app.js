c5e57c46b78fb6e0416c5361e66246793166e2ac
YANGSANGHEE <109570278+YANGSANGHEE@users.noreply.github.com>
2022-10-20 10:22:54 +0900

    충돌 걷어냄

--- backend/app.js
+++ backend/app.js
@@ -1,52 +1,28 @@
 const express = require("express");
-const app = express();
-const Main = require('./Server/Router/Main/index');
-const Market = require('./Server/Router/Market/index');
 const path = require('path');
 const cors = require('cors');
 const fs = require('fs');
-const Product = require("./Server/Router/Product/index");
-const uploadTest = require("./Server/Router/uploadTest");
-const orders = require("./Server/Router/Orders/index");
-const cart = require("./Server/Router/Cart/index");
 const multer = require("multer");
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
-const login = require("./Server/Router/Login/index");
 
+const Main = require('./Server/Router/Main/index');
+const Product = require("./Server/Router/Product/index");
+const uploadTest = require("./Server/Router/uploadTest");
+const recipe = require("./Server/Router/Recipe/index");
+const login = require("./Server/Router/Login/index");
+const signUp = require("./Server/Router/SignUp/index");
+const Market = require('./Server/Router/Market/index');
+const orders = require("./Server/Router/Orders/index");
+const cart = require("./Server/Router/Cart/index");
 const PORT = process.env.PORT || 8080;
 
-try {
-  fs.readdirSync("uploads");
-} catch (error) {
-  fs.mkdirSync("uploads");
-  console.log("uploads 파일이 없어서 생성합니다!");
-}
+const app = express();
 
-// Request full drive access.
-
-// const upload = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     cb(null, "uploads/");
-//   },
-//   filename: (req, file, cb) => {
-//     const ext = path.extname(file.originalname);
-//     cb(null, file.fieldname + "-" + Date.now() + ext);
-//   },
-//   limits: { fileSize: 5 * 1024 * 1024 },s
-// });
-
-const upload = multer({
-  storage: multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, "uploads");
-    },
-    filename: function (req, file, cb) {
-      cb(null, new Date().valueOf() + path.extname(file.originalname));
-    },
-  }),
-});
-
+app.use(express.static(path.join(__dirname, "uploads")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser("keyboard cat"));
 
 app.use(
   cors({
@@ -54,13 +30,7 @@ app.use(
     credentials: true, // 사용자 인증이 필요한 리소스(쿠키 ..등) 접근
   })
 );
-app.use('/', Main);
-app.use('/market', Market);
-app.use(express.static(path.join(__dirname, "uploads")));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 
-app.use(cookieParser("keyboard cat"));
 app.use(
   session({
     secret: "keyboard cat",
@@ -75,11 +45,33 @@ app.use(
 
 app.use("/", Main);
 app.use("/product", Product);
-app.use("/login", login);
 app.use("/upload", uploadTest);
+app.use("/recipe", recipe);
+app.use("/login", login);
+app.use("/signUp", signUp);
 app.use("/orders", orders);
 app.use("/cart", cart);
-app.use("/store")
+app.use('/market', Market);
+
+
+try {
+  fs.readdirSync("uploads");
+} catch (error) {
+  fs.mkdirSync("uploads");
+  console.log("uploads 파일이 없어서 생성합니다!");
+}
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "uploads");
+    },
+    filename: function (req, file, cb) {
+      cb(null, new Date().valueOf() + path.extname(file.originalname));
+    },
+  }),
+});
+
 
 app.post("/upload", upload.single("img"), (req, res) => {
   console.log(req.file);
