eb3edf199038fa34289c3bd467bd9325c0b358cb
yeonjujoeng0511 <yeonjujeong0511@gmail.com>
2022-10-20 11:19:52 +0900

    회원가입 수정중

--- frontend/src/Pages/SignUp/signUp.js
+++ frontend/src/Pages/SignUp/signUp.js
@@ -1,6 +1,7 @@
-import styled from "styled-components";
-import axios from "axios";
-import { useEffect, useState } from "react";
+import styled from 'styled-components';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 const SignUpParent = styled.form`
   /* 회원가입창 부모 설정 */
   width: 1230px;
@@ -256,10 +257,63 @@ const InputClick = styled.input`
 
 const SignUp = () => {
   const [data, setData] = useState();
+
+  const [inputData, setInputData] = useState({
+    userAuth: '',
+    userId: '',
+    userEmail: '',
+    userPassword: '',
+    userPasswordcheck: '',
+    userName: '',
+    userZipcode: '',
+    userAddress: '',
+    userAddressDetail: '',
+    userCall: '',
+    userPhone: '',
+    useBrith: '',
+  });
+
+  const {
+    userAuth,
+    userId,
+    userEmail,
+    userPassword,
+    userName,
+    userZipcode,
+    userAddress,
+    userAddressDetail,
+    userCall,
+    userPhone,
+    useBrith,
+  } = inputData;
+
+  const onchange = (e) => {
+    const { value, name } = e.target;
+    setInputData({
+      ...inputData,
+      [name]: value,
+    });
+  };
+
+  const dbData = {
+    userId: userId,
+    userAuth: userAuth,
+    userEmail: userEmail,
+    userPassword: userPassword,
+    userName: userName,
+    userZipcode: userZipcode,
+    userAddress: userAddress,
+    userAddressDetail: userAddressDetail,
+    userCall: userCall,
+    userPhone: userPhone,
+    useBrith: useBrith,
+  };
+
+  console.log(dbData);
   useEffect(() => {
     const getData = async () => {
       try {
-        let response = await axios.get("http://localhost:8080/signUp");
+        let response = await axios.get('http://localhost:8080/signUp');
         setData(response.data);
       } catch (error) {
         console.log(error);
@@ -271,7 +325,7 @@ const SignUp = () => {
     return null;
   }
   return (
-    <SignUpParent action="http://localhost:8080/signUp" method="post">
+    <SignUpParent action='http://localhost:8080/signUp' method='post'>
       {/* 회원가입창 부모 설정 */}
       <h1>회원가입</h1>
       <br />
@@ -282,10 +336,10 @@ const SignUp = () => {
           회원구분<span>*</span>
         </SideBar>
         <div>
-          <InputClick type={"radio"} name="member"></InputClick>
+          <InputClick type='radio' name='member' value='일반'></InputClick>
           {/* 선택 버튼 */}
           &nbsp;개인회원 &nbsp;&nbsp;
-          <InputClick type={"radio"} name="member"></InputClick>
+          <InputClick type='radio' name='member' value='사업자'></InputClick>
           &nbsp;사업자회원
         </div>
       </MemberType>
@@ -299,7 +353,7 @@ const SignUp = () => {
             {/* 왼쪽 목록 작은 사이즈 */}
             아이디<span>*</span>
           </MiddleBox>
-          <InputText type="text" name="id"></InputText>
+          <InputText type='text' name='userId' onChange={onchange}></InputText>
           {/* 작성란 */}
           <p>(영문 대소문자/숫자/특수문자 중 2가지 이상 조합, 10자~16자)</p>
         </div>
@@ -307,44 +361,66 @@ const SignUp = () => {
           <MiddleBox>
             비밀번호<span>*</span>
           </MiddleBox>
-          <InputText type="text" name="password"></InputText>
+          <InputText
+            type='password'
+            name='userPassword'
+            onChange={onchange}
+          ></InputText>
         </div>
         <div>
           <MiddleBox>
             비밀번호 확인<span>*</span>
           </MiddleBox>
-          <InputText></InputText>
+          <InputText type='password'></InputText>
         </div>
         <div>
           <MiddleBox>
             이름<span>*</span>
           </MiddleBox>
-          <InputText type={"text"} name="name"></InputText>
+          <InputText
+            type='text'
+            name='userName'
+            onChange={onchange}
+          ></InputText>
         </div>
         <div>
           <LongBox>주소</LongBox>
           {/* 왼쪽 목록 큰 사이즈 */}
           <div>
             <div>
-              <input type="text" placeholder="우편번호"></input>
+              <input
+                type='text'
+                placeholder='우편번호'
+                name='userZipcode'
+              ></input>
               &nbsp;&nbsp;&nbsp;
               <button>주소검색</button>
             </div>
             <br />
             <div>
-              <InputText placeholder="기본주소"></InputText>
+              <InputText
+                placeholder='기본주소'
+                name='userAddress'
+                onChange={onchange}
+              ></InputText>
               <br />
               <br />
-              <InputText placeholder="나머지 주소(선택 입력 가능)"></InputText>
+              <InputText
+                placeholder='나머지 주소(선택 입력 가능)'
+                name='userAddressDetail'
+                onChange={onchange}
+              ></InputText>
             </div>
           </div>
         </div>
         <div>
           <MiddleBox>일반전화</MiddleBox>
           <Phone
-            type={"number"}
+            type='number'
             placeholder="전화번호 입력('-'제외)"
             maxLength={11}
+            name='userCall'
+            onChange={onchange}
           />
         </div>
         <div>
@@ -352,10 +428,11 @@ const SignUp = () => {
             휴대전화<span>*</span>
           </MiddleBox>
           <Phone
-            type={"text"}
+            type={'text'}
             placeholder="핸드폰번호 입력('-'제외)"
             maxLength={11}
-            name="phone"
+            name='userPhone'
+            onChange={onchange}
           />
           {/* 폰번호 작성란 */}
           &nbsp;&nbsp;&nbsp;
@@ -365,7 +442,7 @@ const SignUp = () => {
           <MiddleBox>
             이메일<span>*</span>
           </MiddleBox>
-          <InputText name="email"></InputText>
+          <InputText name='userEmail' onChange={onchange}></InputText>
         </div>
       </BasicInfo>
       <br />
@@ -375,16 +452,16 @@ const SignUp = () => {
         {/* 추가정보 */}
         <div>
           <MiddleBox>생년월일</MiddleBox>
-          <input type={"date"}></input>
+          <input type='date' name='userBrith' onChange={onchange}></input>
           &nbsp;&nbsp;&nbsp;
-          <InputClick type={"radio"} name="calendar" />
+          <InputClick type={'radio'} name='calendar' />
           &nbsp;양력 &nbsp;&nbsp;
-          <InputClick type={"radio"} name="calendar" />
+          <InputClick type={'radio'} name='calendar' />
           &nbsp;음력
         </div>
         <div>
           <MiddleBox>추천인 아이디</MiddleBox>
-          <InputText type={"search"}></InputText>
+          <InputText type={'search'}></InputText>
         </div>
       </MoreInfo>
       <br />
@@ -393,7 +470,7 @@ const SignUp = () => {
       <Terms>
         {/* 약관동의 */}
         <div>
-          <InputClick type={"checkbox"}></InputClick>
+          <InputClick type={'checkbox'}></InputClick>
           &nbsp;&nbsp;
           <p>
             이용약관 및 개인정보수집 및 이용, 쇼핑정보 수신(선택)에 모두
@@ -401,28 +478,28 @@ const SignUp = () => {
           </p>
         </div>
         <div>
-          <InputClick type={"checkbox"}></InputClick>
+          <InputClick type={'checkbox'}></InputClick>
           &nbsp;&nbsp;
           <p>[필수] 이용약관 동의</p>
         </div>
         <div>
-          <InputClick type={"checkbox"}></InputClick>
+          <InputClick type={'checkbox'}></InputClick>
           &nbsp;&nbsp;
           <p>[필수] 개인정보 수집 및 이용 동의</p>
         </div>
         <div>
-          <InputClick type={"checkbox"}></InputClick>
+          <InputClick type={'checkbox'}></InputClick>
           &nbsp;&nbsp;
           <p>[선택] SMS 수신을 동의하십니까?</p>
           &nbsp;&nbsp;&nbsp;
-          <InputClick type={"checkbox"}></InputClick>
+          <InputClick type={'checkbox'}></InputClick>
           &nbsp;&nbsp;
           <p>[선택] 이메일 수신을 동의하십니까?</p>
         </div>
       </Terms>
       <br />
       <br />
-      <button type="submit">회원가입</button>
+      <button type='submit'>회원가입</button>
     </SignUpParent>
   );
 };
