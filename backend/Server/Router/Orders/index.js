339a031ec9b5172e41a419d1c1243083685129da
LeeSangHo <4leaf.lsh@gmail.com>
2022-10-20 10:30:24 +0900

    add

--- backend/Server/Router/Orders/index.js
+++ backend/Server/Router/Orders/index.js
@@ -2,4 +2,8 @@ const express = require("express");
 const router = express.Router();
 const connection = require("../../../db/db");
 
+router.get("/:id", (req, res) => {
+  console.log(req.params);
+});
+
 module.exports = router;
